de5886b409183ee6a9ce985c126ad02a7bb2b15b
ktrabelsi <ktrabelsi@spark.local>
2023-02-05 20:27:06 +0100

    firstCommit

---
+++ src/main/java/com/example/sonarqubedemo/MessageController.java
@@ -0,0 +1,14 @@
+package com.example.sonarqubedemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+    @GetMapping("/")
+    public String message() {
+        return "hello";
+    }
+}
